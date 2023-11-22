7d65190908b71a33d94e68129ce0528834ad367a
Shahin Eskandari <shahinesi7@gmail.com>
2023-11-22 14:59:20 +0330

    fix bug

--- asset/js/main.js
+++ asset/js/main.js
@@ -192,11 +192,10 @@ $('.tab ul.tabs li a').on('click', function (g) {
 function order() {
     if (localStorage.getItem('fullName') == null || localStorage.getItem('fullName') == "null") {
         localStorage.setItem('fullName', prompt('لطفا نام و نام خانوادگی خود را وارد کنید'))
-    } else if (localStorage.getItem('phone') == null || localStorage.getItem('phone') == "null") {
-        localStorage.setItem('phone', prompt('لطفا شماره همراه خود را وارد کنید'))
+    } else if (localStorage.getItem('phoneNumber') == null || localStorage.getItem('phoneNumber') == "null") {
+        localStorage.setItem('phoneNumber', prompt('لطفا شماره همراه خود را وارد کنید'))
     } else {
         let date = new Date();
-        console.log(localStorage.getItem('phone'))
         fetch('https://api2.ippanel.com/api/v1/sms/pattern/normal/send', {
             method: 'POST',
             headers: {
@@ -212,7 +211,7 @@ function order() {
                     "date": "تاریخ " + date.toLocaleDateString('fa-IR') + " و در ساعت " + date.getHours() + ":" + date.getMinutes(),
                     "product_list": product_list,
                     "price": all_price,
-                    "phone": localStorage.getItem('phone').toString()
+                    "phone": localStorage.getItem('phoneNumber')
                 }
             })
         })
@@ -259,6 +258,6 @@ function order() {
 
     if (localStorage.getItem('fullName') == null || localStorage.getItem('fullName') == "null")
         localStorage.setItem('fullName', prompt('لطفا نام و نام خانوادگی خود را وارد کنید'))
-    if (localStorage.getItem('phone') == null || localStorage.getItem('phone') == "null")
-        localStorage.setItem('phone', prompt('لطفا شماره همراه خود را وارد کنید'))
+    if (localStorage.getItem('phoneNumber') == null || localStorage.getItem('phoneNumber') == "null")
+        localStorage.setItem('phoneNumber', prompt('لطفا شماره همراه خود را وارد کنید'))
 })();
